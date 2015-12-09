ec568580782feccbb61ba524feabb769444a8b84
ricardolupo <rlupo@getchef.com>
2015-12-09 18:31:30 -0500

    updated to include dummy integration to demo kitchen verify

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'desjardins::default' do
   # Serverspec examples can be found at
   # http://serverspec.org/resource_types.html
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
+  it 'is awesome' do
+    expect(true).to eq true
   end
 end
